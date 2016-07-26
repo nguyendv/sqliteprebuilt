1fa8d7d076e7cdce3fc41019e71a02f2e52e75c1
Dung Viet Nguyen <dvnguyen.vn@gmail.com>
2016-07-25 19:19:26 -0500

    Use libc++_shared instead of gnustl_shared

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -3,11 +3,11 @@ APP_ABI := all # Support all abi
 # Android NDK Native APIs
 # Must not greater than the minSdkVersion
 # See here for list of NDK Native APIs: https://developer.android.com/ndk/guides/stable_apis.html
-APP_PLATFORM := android-15 
+APP_PLATFORM := android-16
 
 # Standard C++ runtime
 # See here forthe list of run time: https://developer.android.com/ndk/guides/cpp-support.html#runtimes
 # Read the "Shard runtimes" section for important notes if you choose to use a shared runtime
-APP_STL := gnustl_shared
+APP_STL := c++_shared 
 
 APP_CPPFLAGS += -std=c++11
